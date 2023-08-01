0b761dc6efda3081d04d6efcda30cbcb981d6d04
Snake <stop@mail.ru>
2023-08-01 12:12:18 +0300

    task 25

---
+++ task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число A: ");
+int A=int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B=int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($" Если число {A} возвести в степень {B}, получиться {funct(A,B)}.");
+
+int funct (int A, int B){
+int res=A;
+int i=2;
+while(i<=B){
+    res*=A;
+    i++;
+}
+return res;
+}

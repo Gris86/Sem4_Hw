cb5666e62d7a799c861fc51d0175c049bd79eb8a
Snake <stop@mail.ru>
2023-08-01 12:20:06 +0300

    task 27

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int num=int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {num} равна {sum1(num)}.");
+
+
+
+int sum1(int number){
+    int n=number;
+    int res=0;
+    while(n>9){
+        res+=n%10;
+        n=n/10;
+    }
+    res+=n;
+    return res;
+}
+
+
+
